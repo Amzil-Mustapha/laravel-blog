454fd8cce1e20a0908596fbd423a50462291ee84
Amzil Mustapha <mustaphaamzil124@gmail.com>
2023-03-23 14:28:55 +0100

    initial commit and initialisation of react and inertia

---
+++ database/migrations/2023_03_23_131232_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('img');
+            $table->foreignId('user_id')->constrained();
+            $table->string('category');
+            $table->integer('views');
+            $table->integer('nbr_likes');
+            $table->integer('nbr_dislikes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
---
+++ database/migrations/2023_03_23_131245_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('body');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('article_id')->constrained();
+            $table->integer('nbr_likes');
+            $table->integer('nbr_dislikes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
